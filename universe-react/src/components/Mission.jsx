bf378bbbef2da76fc368b1cac3d5db38fbd0c6e2
Erick-Andrade-Dev <erick.dev07@gmail.com>
2022-09-07 10:27:34 -0300

    add header

---
+++ universe-react/src/components/Mission.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Missions extends React.Component {
+    render() {
+        // const { name, year, country, destination } = this.props
+        return (
+            <div>
+                <p>Name</p>
+                <div>
+                    <p>Year</p>
+                    <p>Country</p>
+                    <p>Destination</p>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ universe-react/src/data/planet.js
@@ -0,0 +1,12 @@
+const Planets = [
+    { name: 'Mercúrio', image:'https://www.iquilibrio.com/blog/wp-content/uploads/2017/01/64607-veja-como-e-mercurio-na-astrologia-1.jpg'},
+    { name: 'Vênus', image:'https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2022/08/05/1652707083-planeta-venus.jpg'},
+    { name: 'Terra', image:'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/The_Blue_Marble_%28remastered%29.jpg/300px-The_Blue_Marble_%28remastered%29.jpg'},
+    { name: 'Marte', image:'https://s1.static.brasilescola.uol.com.br/be/2021/11/planeta-marte.jpg'},
+    { name: 'Júpiter', image:'https://static.poder360.com.br/2019/08/Jupiter-Nasa.png'},
+    { name: 'Saturno', image: 'https://static.preparaenem.com/2021/01/saturno.jpg'},
+    { name: 'Urano', image:'https://realidadesimulada.com/wp-content/uploads/2017/09/Urano.jpg'},
+    { name: 'Netuno', image: 'https://s4.static.brasilescola.uol.com.br/be/2022/08/1-planeta-netuno.jpg'},
+  ];
+  
+  export default Planets;
